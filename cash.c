734ff7dc692ca4ad19ac3d312ce647e445310fa1
Daniela Vaseva <dadano@abv.bg>
2019-10-07 16:26:54 +0300

    remove function

--- cash.c
+++ cash.c
@@ -11,26 +11,61 @@ int main(void)
     {
         change = get_float("Change owed ");
     } 
-    while (change < 1);
+    while (change > 1);
     
     int cents = round(change * 100);
     //printf("%i\n", cents); 
     
-    int cmincoins = mincoins(cents);
-    printf("%i\n", cmincoins);
+    int result = 0;
+    int currentnum1 = cents;
+    //printf("%i\n", currentnum1);
+    while (currentnum1 >= 25)
+    {
+        // Do something
+        currentnum1 = currentnum1 - 25;
+        result ++;
+    }
+    
+    while (currentnum1 >= 10)
+    {
+        // Do something
+        currentnum1 = currentnum1 - 10;
+        result ++;
+    }
+    
+    while (currentnum1 >= 5)
+    {
+        // Do something
+        currentnum1 = currentnum1 - 5;
+        result ++;
+    }
+    
+    while (currentnum1 >= 1)
+    {
+        // Do something
+        currentnum1 = currentnum1 - 1;
+        result ++;
+    }
+    
+    printf("%i\n", result);
+    
+    //int cmincoins;
+    //cmincoins = mincoins(cents);
+    //printf("%i\n", cmincoins);
 }
 
 int mincoins(int num1) {
 
    /* local variable declaration */
    int result = 0;
-    int currentnum1 = num1;
-    while (currentnum1 >= 25)
-    {
+   int currentnum1 = num1;
+   while (currentnum1 >= 25)
+   {
         // Do something
         currentnum1 = num1 - 25;
         result ++;
     }
+    printf("%i\n", result);
     
     while (currentnum1 >= 10)
     {
